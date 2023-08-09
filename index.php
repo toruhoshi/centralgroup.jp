0a5186198a24157080e421157e5cdf088e9293a8
toruhoshi <126557066+toruhoshi@users.noreply.github.com>
2023-08-09 16:59:53 +0900

    Add files via upload
    グループ創業:文言変更

---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width">
+		<title>企業概要｜会社概要｜パチンコセントラルグループ</title>
+		<meta name="Description" content="東北を中心に8店舗を展開するパチンコチェーン、セントラルグループの企業概要です。" />
+		<meta name="keywords" content="パチンコ,セントラル,パチンコセントラル,パチスロ,スロット,central,4パチ,1パチ,5スロ" />
+		<link rel="shortcut icon" href="../favicon.ico">
+		<!-- fb st -->
+		<meta property="og:title" content="企業概要｜会社概要｜パチンコセントラルグループ">
+		<meta property="og:description" content="東北を中心に8店舗を展開するパチンコチェーン、セントラルグループの企業概要です。">
+		<meta property="og:type" content="article">
+		<meta property="og:image" content="">
+		<meta property="og:url" content="http://www.centralgroup.jp/corporate/">
+		<!-- css -->
+		<link rel="stylesheet" href="../css/reset.css">
+		<link rel="stylesheet" media="(min-width:768px)" href="../css/addstyle.css">
+		<link rel="stylesheet" media="(max-width:767px)" href="../css/addstyle-sp.css">
+	</head>
+	<body class="page-corporate" id="top">
+		<?php include("../include/header.html");?>
+		<div class="contents-wrap">
+			<div class="inr">
+				<div class="bread">
+					<ol itemscope itemtype="http://data-vocabulary.org/Breadcrumb" class="list-bread">
+						<li><a href="../" itemprop="url"><span itemprop="title">TOP</span></a>&nbsp;&gt;&nbsp;</li>
+						<li>企業概要</li>
+					</ol>
+				</div>
+			</div>
+
+			<?php include("../include/hero.html");?>
+
+			<div class="inner">
+				<div class="layout-wrap-twocolumn lower">
+					<div class="layout-main">
+						<div class="main-white">
+							<h1 class="main-title"><img src="../img/corporate/title02.jpg" width="700" height="auto" alt="Corporate 企業概要"></h1>
+							<h2 class="sec-title">企業概要</h2>
+							<table class="table-base">
+								<tr>
+									<th scope="row">会社名</th>
+									<td>株式会社セントラル伸光</td>
+								</tr>
+								<tr>
+									<th scope="row">設立</th>
+									<td>昭和57年9月11日</td>
+								</tr>
+								<tr>
+									<th scope="row">本社</th>
+									<td>〒 980-0002　宮城県仙台市青葉区福沢町3-33</td>
+								</tr>
+								<tr>
+									<th scope="row">URL</th>
+									<td>http://www.centralgroup.jp/</td>
+								</tr>
+								<tr>
+									<th scope="row">代表者</th>
+									<td>代表取締役社長：清水 文徳</td>
+								</tr>
+								<tr>
+									<th scope="row">グループ創業</th>
+									<td>昭和32年10月10日</td>
+								</tr>
+								<tr>
+									<th scope="row">事業内容</th>
+									<td>遊技場経営(パチンコセントラルの経営・運営)</td>
+								</tr>
+								<tr>
+									<th scope="row">資本金</th>
+									<td>2,575万</td>
+								</tr>
+								<tr>
+									<th scope="row">社員数</th>
+									<td>320名</td>
+								</tr>
+							</table>
+						</div>
+					</div>
+					<!-- layput-main -->
+					<div class="layout-side">
+						<div class="side-title">
+							<p>会社概要</p>
+						</div>
+						<ul class="list-side-menu">
+							<li class="current"><a href="./">企業概要</a></li>
+							<li><a href="./policy.php">企業理念</a></li>
+							<li><a href="./history.php">企業沿革</a></li>
+						</ul>
+					</div>
+					<!-- layout-side -->
+				</div>
+				<!-- layout-wrap-twocolumn -->
+			</div>
+		</div>
+		<!-- contents-wrap -->
+		<?php include("../include/footer.html");?>
+	</body>
+</html>
